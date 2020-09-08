cc8f586b55e01ee235186b220cc78674d9e1f6df
Nastya <nastjasokolovaa@gmail.com>
2020-09-08 22:15:33 +0300

    Homework 003

--- python/homework/task03.py
+++ python/homework/task03.py
@@ -5,3 +5,31 @@
 # Если число делится нацело на 5, вместо числа выводится «Buzz».
 # Если число делится нацело и на 3 и на 5, вместо числа выводится «FizzBuzz».
 # Во всех остальных случаях выводится само число.
+
+
+for i in range(1, 101):
+    ok = False
+    if i % 3 == 0:
+        ok = True
+        print('Fizz', end='')
+    if i % 5 == 0:
+        ok = True
+        print('Buzz', end='')
+
+    if not ok:
+        print(i, end='')
+
+    if i != 100:
+        print(',', end='')
+
+    # for i in range(1, 101):
+    #     if i == 100:
+    #         print('Buzz')
+    #     elif i % 3 == 0 and i % 5 == 0:
+    #         print('FizzBuzz', end=',')
+    #     elif i % 3 == 0:
+    #         print('Fizz', end=',')
+    #     elif i % 5 == 0:
+    #         print('Buzz', end=',')
+    #     else:
+    #         print(i, end=',')
